d20a70fc7b643c9c01cee69bcbcd9d23f02b9e98
Andreia Lima <64980482+AndreiaLCoding@users.noreply.github.com>
2021-07-11 16:11:30 -0400

    set function variables and window confirmations

--- password-generator/friendly-parakeet/Develop/script.js
+++ password-generator/friendly-parakeet/Develop/script.js
@@ -4,6 +4,33 @@ var specialCharacters = ['!','"','#','$','%','&',"'",'&','(',')', '*', '+', ',',
 var UpperABCs=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var LowerABCs = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
+function randomNum (min,max){
+  var value = Math.floor(Math.random()) * (max-min + 1) + min);
+  return value;
+};
+
+function createPassword(){
+  var specialCharacters = []
+  var password=[]
+  var characterCount = 10;
+  var characterCount = window.prompt ("Please select number of Characters between from 1 to 10")
+  var characterCount = parseInt(characterCount)
+
+  while (characterCount > 128 || characterCount <=8 || isNaN (characterCount)){
+    alert('Please select a password between 8 and 28 characters.')
+    characterCount = parseInt(characterCount)
+  }
+}
+
+var yesToNum = window.confirm ('Would you like numbers in your password?');
+var yesToSpecialCh= window.confirm ('Would you like special characters in your password?')
+var yesToUpper = window.confirm('Would you want uppercase letters in your password?');
+var yesToLower = window.confirm("Would you like lowercase letters in your password?")
+
+if (yesToNum) {
+  charac = charac.contact(specialCharacters)
+};
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
